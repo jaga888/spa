60eea1066b84fffbddb0712fbd346798a7c2b043
Vitali Golubka <jokkerit3@gmail.com>
2024-01-05 17:39:16 +0100

    master | temporary commit

---
+++ layouts/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {definePageMeta} from "nuxt/dist/pages/runtime";
+
+definePageMeta({
+  middleware: 'auth'
+})
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <p>This page will be displayed at the /login page.</p>
+  </section>
+</template>
